965a17019323fde3f09fbd85dba635eeebe2a5b7
Guillermo Ortiz <guillermortiz21@gmail.com>
2025-03-10 21:04:06 -0600

    Contribución

--- modules/module_data.py
+++ modules/module_data.py
@@ -80,6 +80,8 @@ class Dataset:
 
         # Sample
         if self.num_samples is not None:
-            train_combined = train_combined.sample(self.num_samples, random_state=self.random_seed)        
+            train_combined = train_combined.sample(self.num_samples, random_state=self.random_seed)
+        
+        print("hello world!")
 
         return train_combined, test_combined, labels
